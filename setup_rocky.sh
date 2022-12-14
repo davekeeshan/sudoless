e4b399edcf5c306d47965d2f3d8b2eee99b0b223
Dave Keeshan <dkeeshan@daxzio.com>
2022-12-14 05:38:29 -0500

    Fix some dependancies

--- setup_rocky.sh
+++ setup_rocky.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/bash
 sudo dnf update -y
-sudo dnf install make  -y
+sudo dnf install make tigervnc-server -y
 sudo dnf groupinstall 'Development Tools' -y
+#sudo dnf install perl-IPC-Cmd perl-Pod-Html -y
+sudo dnf install libX11-devel-1.6.8-5.el8.x86_64 -y
+
+sudo dnf install autoconf automake python3 libmpc-devel mpfr-devel gmp-devel gawk bison flex patchutils gcc gcc-c++ zlib-devel expat-devel -y
+sudo dnf install texinfo -y
 
 ./setup_groups.sh
