9d228f9e1a4936272ede2aef67477ca0f1a5ee49
Dave Keeshan <dave.keeshan@daxzio.com>
2022-12-16 10:13:15 +0000

    Add user to sudo group

--- setup_groups.sh
+++ setup_groups.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/bash
+git config --global user.email "dave.keeshan@daxzio.com"
+git config --global user.name "Dave Keeshan"
+
+sudo usermod -G sudo ${USER}
+
 NEWGRP=cadtools
 sudo groupadd -g 2000 ${NEWGRP}
 sudo usermod -G ${NEWGRP} ${USER}
