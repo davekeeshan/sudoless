34a32e146b678b0ea64a19efc57a0d56cbe81a32
Dave Keeshan <dave.keeshan@daxzio.com>
2023-01-25 14:06:46 +0000

    More includes

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -4,7 +4,8 @@ sudo apt install make build-essential -y
 sudo apt install autoconf automake autotools-dev curl libmpc-dev libmpfr-dev libgmp-dev gawk bison flex texinfo gperf libtool patchutils bc zlib1g-dev libexpat-dev -y
 sudo apt install libx11-dev libxt-dev libmotif-dev libxpm-dev libpcre3-dev -y
 sudo apt install tigervnc-standalone-server tigervnc-xorg-extension tigervnc-viewer -y
-
-# sudo apt install device-tree-compiler -y
+sudo apt install nedit openssh-server nfs-common -y
+sudo apt install libncurses5 libtinfo5 libncurses5-dev libncursesw5-dev ncurses-compat-libs -y
+ # sudo apt install device-tree-compiler -y
 
 ./setup_groups.sh
